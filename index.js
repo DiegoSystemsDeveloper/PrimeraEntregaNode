7b490cd2d7dbd1c184a7da597d69a0f49b8b3004
diego <hhola3628@hmail.com>
2019-06-02 12:51:43 -0500

    express

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cursos = {cursosDisponibles, mostrarCursos, cursoBuscado} = require ('./Cursos');
-const fs = require ('fs');
+const express = require('express')
+const app = express()
 
 //Arreglo que guardará la informacion del usuario que se ingrese por teclado
 const usuario = {
@@ -21,38 +22,44 @@ const argv = require ('yargs')
 			 .command('inscribir','Inscribir usuario', usuario)
 			 .argv
 
-//Funcion para crear archivo de texto con la informacion del usuario y el curso que ha escogido
-function crearArchivo() {
-	texto = ('Informacion del interesado: ' + '\n' +
-			'Nombre: ' + argv.nombre + '\n' +
-			'Cedula: ' + argv.cedula + '\n\n' +
-			'Informacion del curso: ' + '\n' +
-			'Nombre del curso: ' + cursosDisponibles[argv.curso-1].nombre + '\n' +
-			'Id del curso: ' + cursosDisponibles[argv.curso-1].id + '\n' +
-			'Duracion del curso: ' + cursosDisponibles[argv.curso-1].horas + '\n' +
-			'Valor del curso: ' + cursosDisponibles[argv.curso-1].valor);
-			fs.writeFile('Informacion.txt', texto, (err) => {
-			if(err) throw (err);
-			console.log('Se ha registrado correctamente la informacion');
-			});
+let imprimirEnPantalla = (texto) => {
+	app.get('/', function (req, res) {
+  			res.send('<b> ' + texto + '</b>')
+			})
 }
 
-//Metodo que ejecuta el programa segun la histroria de usuario indicada
+//Funcion para mostrar en pantalla la informacion del usuario y el curso que ha escogido
+function mostrarCursoSeleccionado() {
+	texto = ('Informacion del interesado: ' + '<br>' +
+			'Nombre: ' + argv.nombre + '</br>' +
+			'Cedula: ' + argv.cedula + '</br></br>' +
+			'Informacion del curso: ' + '</br>' +
+			'Nombre del curso: ' + cursosDisponibles[argv.curso-1].nombre + '</br>' +
+			'Id del curso: ' + cursosDisponibles[argv.curso-1].id + '</br>' +
+			'Duracion del curso: ' + cursosDisponibles[argv.curso-1].horas + '</br>' +
+			'Valor del curso: ' + cursosDisponibles[argv.curso-1].valor);
+			imprimirEnPantalla(texto);
+}
+
+//Metodo que ejecuta el programa segun la historia de usuario indicada
 function inicializar() {
 	if(argv.curso == null || argv.nombre == null || argv.cedula == null) {
 		mostrarCursos(2000, 4000, 6000);
+		imprimirEnPantalla('Aun no se ha seleccionado un curso');
 	}
 	else {
 		if(cursoBuscado(argv.curso) == null) {
 			console.log('El curso con el id [' + argv.curso + '] no existe.');
+			imprimirEnPantalla('Aun no se ha seleccionado un curso')
 			mostrarCursos(2000, 4000, 6000);
 		} else{
 			console.log(cursoBuscado(argv.curso));
 			setTimeout(function () {
-			crearArchivo();
+			mostrarCursoSeleccionado();
 			}, 2000);
 		}
 	}	
 }
 
 inicializar();
+app.listen(3000);
